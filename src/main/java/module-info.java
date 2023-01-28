56b0c43731e6369c6eac10a8588b1a023f6b5b72
2b33rs <tobiasbiehl@icloud.com>
2023-01-28 17:03:18 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module ml.pils.pomowatch {
+    requires javafx.controls;
+    requires javafx.fxml;
+        requires javafx.web;
+            
+        requires org.controlsfx.controls;
+            requires com.dlsc.formsfx;
+            requires net.synedra.validatorfx;
+            requires org.kordamp.ikonli.javafx;
+            requires org.kordamp.bootstrapfx.core;
+            requires eu.hansolo.tilesfx;
+            requires com.almasb.fxgl.all;
+    
+    opens ml.pils.pomowatch to javafx.fxml;
+    exports ml.pils.pomowatch;
+}
