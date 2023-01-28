f1ec95e04379a150ad9b2d9935445479c15657ab
2b33rs <tobiasbiehl@icloud.com>
2023-01-28 17:41:55 +0100

    add Button "Start Timer"

--- src/main/java/ml/pils/pomowatch/HelloController.java
+++ src/main/java/ml/pils/pomowatch/HelloController.java
@@ -1,14 +1,36 @@
 package ml.pils.pomowatch;
 
+import javafx.animation.KeyFrame;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.util.Timer;
+
+import static java.lang.Thread.sleep;
+
 public class HelloController {
+    public Label timer;
     @FXML
     private Label welcomeText;
+    private boolean semaphore;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onTimerButtonClick() throws InterruptedException {
+        //start a timer here
+        semaphore = !semaphore;
+
+        KeyFrame keyFrame = new KeyFrame(javafx.util.Duration.seconds(1), event -> {
+            long time = getTimeInSeconds(timer);
+            time++;
+            timer.setText(time / 3600 + ":" + time / 60 + ":" + time % 60);
+        });
+
+    }
+
+    private long getTimeInSeconds(Label timer) {
+
+        String[] time = timer.getText().split(":");
+        return Integer.parseInt(time[0]) * 3600L + Integer.parseInt(time[1]) * 60L + Integer.parseInt(time[2]);
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,15 @@
 module ml.pils.pomowatch {
     requires javafx.controls;
     requires javafx.fxml;
-        requires javafx.web;
-            
-        requires org.controlsfx.controls;
-            requires com.dlsc.formsfx;
-            requires net.synedra.validatorfx;
-            requires org.kordamp.ikonli.javafx;
-            requires org.kordamp.bootstrapfx.core;
-            requires eu.hansolo.tilesfx;
-            requires com.almasb.fxgl.all;
-    
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
     opens ml.pils.pomowatch to javafx.fxml;
     exports ml.pils.pomowatch;
 }
