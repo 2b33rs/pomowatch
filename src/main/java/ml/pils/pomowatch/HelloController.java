5fb012220270c85216b90ce0c52e07f52205f1bb
2b33rs <tobiasbiehl@icloud.com>
2023-01-29 11:14:14 +0100

    add stopwatch function

--- src/main/java/ml/pils/pomowatch/HelloController.java
+++ src/main/java/ml/pils/pomowatch/HelloController.java
@@ -1,7 +1,9 @@
 package ml.pils.pomowatch;
 
 import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 import java.util.Timer;
@@ -10,22 +12,29 @@ import static java.lang.Thread.sleep;
 
 public class HelloController {
     public Label timer;
+    public Button control_btn;
     @FXML
-    private Label welcomeText;
     private boolean semaphore;
+    Timeline timeline;
 
 
     @FXML
-    protected void onTimerButtonClick() throws InterruptedException {
+    protected void onTimerButtonClick() {
         //start a timer here
         semaphore = !semaphore;
-
-        KeyFrame keyFrame = new KeyFrame(javafx.util.Duration.seconds(1), event -> {
-            long time = getTimeInSeconds(timer);
-            time++;
-            timer.setText(time / 3600 + ":" + time / 60 + ":" + time % 60);
-        });
-
+        if (semaphore) {
+            timeline = new Timeline(new KeyFrame(javafx.util.Duration.seconds(1), event -> {
+                long time = getTimeInSeconds(timer);
+                time++;
+                timer.setText(time / 3600 + ":" + time / 60 + ":" + time % 60);
+            }));
+            timeline.setCycleCount(Timeline.INDEFINITE);
+            timeline.play();
+            control_btn.setText("Stop!");
+        } else {
+            timeline.stop();
+            control_btn.setText("Start!");
+        }
     }
 
     private long getTimeInSeconds(Label timer) {
