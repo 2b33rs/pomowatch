7c1e51bce7eadf693893cc81bf9af59665e1815a
2b33rs <tobiasbiehl@icloud.com>
2023-01-30 13:12:42 +0100

    add remove stop function

--- src/main/java/ml/pils/pomowatch/HelloController.java
+++ src/main/java/ml/pils/pomowatch/HelloController.java
@@ -6,7 +6,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-import java.util.Timer;
+import java.util.concurrent.atomic.AtomicLong;
 
 import static java.lang.Thread.sleep;
 
@@ -22,15 +22,16 @@ public class HelloController {
     protected void onTimerButtonClick() {
         //start a timer here
         semaphore = !semaphore;
+        AtomicLong time = new AtomicLong(0L);
         if (semaphore) {
             timeline = new Timeline(new KeyFrame(javafx.util.Duration.seconds(1), event -> {
-                long time = getTimeInSeconds(timer);
-                time++;
-                timer.setText(time / 3600 + ":" + time / 60 + ":" + time % 60);
+//                long time = getTimeInSeconds(timer);
+                time.getAndIncrement();
+                timer.setText(time.get() / 3600 + ":" + time.get() / 60 + ":" + time.get() % 60);
             }));
             timeline.setCycleCount(Timeline.INDEFINITE);
             timeline.play();
-            control_btn.setText("Stop!");
+            control_btn.setText("Reset!");
         } else {
             timeline.stop();
             control_btn.setText("Start!");
